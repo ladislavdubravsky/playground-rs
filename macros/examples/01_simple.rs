f2cc04df470a8dd285595da2db2a1696351aa43f
Ladislav Dubravsky <dubravsky90@gmail.com>
2025-07-11 21:43:13 +0200

    macros: swaps

--- macros/examples/01_simple.rs
+++ macros/examples/01_simple.rs
@@ -15,6 +15,16 @@ macro_rules! double_statement {
     ($x:stmt) => { $x $x };
 }
 
+macro_rules! swap_statements {
+    ($x:stmt, $y:stmt) => { $y $x };
+}
+
+macro_rules! swap_function_application {
+    ($f:ident ($g:ident ($args:expr) ) ) => {
+        $g($f($args))
+    };
+}
+
 /// Only serves to be able to show my macros expanded:
 /// cargo expand --lib
 fn main() {
@@ -25,9 +35,29 @@ fn main() {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn double() {
+    fn double_statement() {
         let mut x = 0;
         double_statement!(x += 2);
         assert_eq!(x, 4);
+    }
+
+    #[test]
+    fn swap_statements() {
+        let mut x = 0;
+        swap_statements!(x += 3, x *= 2);
+        assert_eq!(x, 3);
+    }
+
+    #[test]
+    fn swap_function_application() {
+        fn incr(x: i32) -> i32 {
+            x + 1
+        }
+        fn double(x: i32) -> i32 {
+            x * 2
+        }
+
+        let res = swap_function_application!(incr(double(3)));
+        assert_eq!(res, 8);
     }
 }
